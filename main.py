49f7c5342fdfe4265976c0710ea2225ff07f636a
Nazmul Hassan <nazmul.hassan.au@gmail.com>
2020-04-10 13:27:27 +1000

    Initail commit

---
+++ main.py
@@ -0,0 +1,18 @@
+msg = 'Hello World Hello'
+print (msg)
+print (type(msg))
+print (len(msg))
+# print (msg[0:5])  # 0=included but 5=not included i.e. 0,1,2,3,4
+# print (msg[:5])    # start for 0 anyway
+# print(msg.lower(),'\n')
+# print(msg.upper())
+# print(msg.count('hello'))
+# print(msg.count('Hello'))
+# print(msg.find('hello'))
+# print ('World' in msg)
+# print(msg, replace('hello','Hellow'))
+print('\n')
+first_str = 'hello'
+second_str = 'world'
+msg = first_str + '\t'  + second_str
+msg = '{} {} we are learning '
